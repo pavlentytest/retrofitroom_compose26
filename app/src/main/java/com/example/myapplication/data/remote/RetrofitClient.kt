2a9dc02d11767559875f47d05b8fce4719d7e59d
Paul S <21249606+pavlentytest@users.noreply.github.com>
2026-04-16 10:36:53 +0300

    Interceptor - all

--- app/src/main/java/com/example/myapplication/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/myapplication/data/remote/RetrofitClient.kt
@@ -2,14 +2,20 @@ package com.example.myapplication.data.remote
 
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
     private val gson = GsonBuilder().create()
     private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(logging)
         .connectTimeout(60, TimeUnit.SECONDS)
         .build()
 
